13926ed1684d3d3d29df87e9a768f4425e8964f4
Vladimir <example@you.com>
2021-12-09 23:24:03 +0300

    6.2.14

--- src/Pair.java
+++ src/Pair.java
@@ -1,71 +1,51 @@
+/*Магазину нужно сохранять информацию о продажах для каждого сотрудника. Напишите метод Map getSalesMap(Reader reader) который принимает Reader содержащий строки вида:
+
+        Алексей 3000
+        Дмитрий 9000
+        Антон 3000
+        Алексей 7000
+        Антон 8000
+        Метод должен получить все строки из Readera и заполнить и вернуть карту где ключом будет имя сотрудника, а значением сумма всех его продаж.
+
+        Пример ввода:
 
 
-/*Реализуйте generic-класс Pair, похожий на Optional, но содержащий пару элементов разных типов и не запрещающий элементам принимать значение null.
+        Алексей 3000
+        Дмитрий 9000
+        Антон 3000
+        Алексей 7000
+        Антон 8000
+        Пример вывода:
+        {Алексей=[10000], Дмитрий=[9000], Антон=[11000]}
 
-        Реализуйте методы getFirst(), getSecond(), equals() и hashCode(), а также статический фабричный метод of(). Конструктор должен быть закрытым (private).
-
-        С корректно реализованным классом Pair должен компилироваться и успешно работать следующий код:
-
-
-        Pair<Integer, String> pair = Pair.of(1, "hello");
-        Integer i = pair.getFirst(); // 1
-        String s = pair.getSecond(); // "hello"
-        Pair<Integer, String> pair2 = Pair.of(1, "hello");
-        boolean mustBeTrue = pair.equals(pair2); // true!
-        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
-        Пожалуйста, не меняйте модификатор доступа класса Pair. Для корректной проверки класс должен иметь пакетную видимость.*/
+        Требования:
+        1. Должен быть метод public static Map<String, Long> getSalesMap(Reader reader)
+        2. Работа метода getSalesMap должна удовлетворять условию*/
 
 
-import java.util.Objects;
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Map;
 
-public  class Pair<T,V> {
-    private T obj1;
-    private V obj2;
-
-    private Pair(T obj1, V obj2) {
-        this.obj1 = obj1;
-        this.obj2 = obj2;
-    }
+public class Pair {
 
     public static void main(String[] args) {
-        Pair<Integer, String> pair = Pair.of(1, "hello");
-        System.out.println(pair);
-        Integer i = pair.getFirst();
-        System.out.println(i);
-        String s = pair.getSecond();
-        System.out.println(s);
-        Pair<Integer, String> pair2 = Pair.of(1, "hello");
-        System.out.println(pair2);
-        boolean mustBeTrue = pair.equals(pair2);
-        System.out.println(mustBeTrue);
-        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode();
-        System.out.println(mustAlsoBeTrue);
-
-    }
-    public static <T,V> Pair<T,V> of(T obj1, V obj2) {
-        return new Pair<>(obj1,obj2);
+    String source = "Алексей 3000\n" +
+            "        Дмитрий 9000\n" +
+            "        Антон 3000\n" +
+            "        Алексей 7000\n" +
+            "        Антон 8000";
+        StringReader s = new StringReader(source);
+        BufferedReader reader = new BufferedReader(s);
+        System.out.println(getSalesMap(reader));
     }
 
-     public T getFirst(){
-        return obj1;
-    };
 
-    public V getSecond(){
-        return obj2;
-    };
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pair<?, ?> pair = (Pair<?, ?>) o;
-        return obj1.equals(pair.obj1) && obj2.equals(pair.obj2);
+    public static Map<String, Long> getSalesMap(Reader reader) {
+        //Твой код здесь
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(obj1, obj2);
-    }
 }
 
 
